326fadec7e5b6a8538d87f581edb89f55b55defc
Viljami S <viljami.salminen@gmail.com>
2013-03-13 16:33:15 +0200

    Make eventlisteners work in IE too

--- TinyNav2.js
+++ TinyNav2.js
@@ -1,4 +1,4 @@
-/*! tinyNav2.js v0.65
+/*! tinyNav2.js v1.0
  * https://github.com/viljamis/tinyNav2.js
  * http://tinynav2.viljamis.com
  *
@@ -6,198 +6,228 @@
  * Available under the MIT license
  */
 
-/*jslint browser: true, sloppy: true, vars: true, plusplus: true, indent: 2 */
+/*jslint forin: true, browser: true, sloppy: true, vars: true, plusplus: true, indent: 2, devel: true, nomen: true */
 
-(function (w) {
+var TinyNav = (function (window, document) {
 
-  var tinyNav = w.tinyNav || {};
-
-  var nav,
+  var c = console,
+    checkResize,
     navToggle,
-    navInner,
-    c = console,
-    doc = w.document,
+    doc = window.document,
     aria = "aria-hidden",
     ua = navigator.userAgent,
+    resizeEvent = 'onorientationchange' in window ? 'orientationchange' : 'resize',
+    computed = window.getComputedStyle ? true : false,
     head = doc.getElementsByTagName("head")[0],
     styleEl = doc.createElement("style"),
-    computed = w.getComputedStyle ? true : false,
-    label = tinyNav.label || "Menu",
-    debug = tinyNav.debug || false; // Boolean: log debug messages to console, true or false
+    nav_open = false,
+    closed = "closed",
+    opened = "opened",
 
-  // Debug
-  if (debug === true) {
-    c.log("Inited Tinynav2.js");
-  }
-
-  // Create style element
-  head.appendChild(styleEl);
-
-  // Debug
-  if (debug === true) {
-    c.log("Created initial 'styleEl' to <head>");
-  }
-
-  // Resizer
-  w.resizer = function () {
-    if (computed) {
-      if (w.getComputedStyle(navToggle, null).getPropertyValue("display") !== "none") {
-
-        // Set aria-hidden
-        navToggle.setAttribute(aria, false);
-        nav.setAttribute(aria, true);
-
-        // Inject custom styles
-        if (!styleEl.parentNode) {
-          head.appendChild(styleEl);
-
-          // Debug
-          if (debug === true) {
-            c.log("Appended 'styleEl' to <head>");
-          }
-
-        }
-        var savedHeight = navInner.offsetHeight,
-          innerStyles = "#nav.opened{max-height:" + savedHeight + "px }";
-        styleEl.innerHTML = innerStyles;
-        innerStyles = '';
-
-        // Debug
-        if (debug === true) {
-          c.log("Calculated max-height of " + savedHeight + " pixels and updated 'styleEl'");
-        }
-
-      } else {
-
-        // Set aria-hidden
-        navToggle.setAttribute(aria, true);
-        nav.setAttribute(aria, false);
-
-        // Remove custom styles
-        if (styleEl.parentNode) {
-          styleEl.parentNode.removeChild(styleEl);
-
-          // Debug
-          if (debug === true) {
-            c.log("Removed 'styleEl' from <head>");
-          }
-
-        }
+    addEvent = function (obj, type, fn) {
+      if (obj.addEventListener) {
+        obj.addEventListener(type, fn, false);
+      } else if (obj.attachEvent) {
+        obj["e" + type + fn] = fn;
+        obj[type + fn] = function () {
+          obj["e" + type + fn](window.event);
+        };
+        obj.attachEvent("on" + type, obj[type + fn]);
       }
-    }
-  };
+    },
 
-  // Navigation
-  w.navigation = function () {
+    removeEvent = function (obj, type, fn) {
+      if (obj.removeEventListener) {
+        obj.removeEventListener(type, fn, false);
+      } else if (obj.detachEvent) {
+        obj.detachEvent("on" + type, obj[type + fn]);
+        obj[type + fn] = null;
+        obj["e" + type + fn] = null;
+      }
+    },
 
-    var nav_open = false,
-      closed = "closed",
-      opened = "opened",
-      getElement = function (el) {
-        return doc.getElementById(el);
+    TinyNav = function (el, options) {
+      var i,
+        navInner;
+
+      // Wrapper
+      this.wrapper = typeof el === "string" ? doc.querySelector(el) : el;
+
+      // Save this for later
+      this.wrapper.tinynav = this;
+
+      // Default settings
+      this.options = {
+        label: "Menu", // String: Label for the navigation toggle, default is "Menu"
+        debug: false, // Boolean: log debug messages to console, true or false
+        navInner: "#nav ul" // Selector: selector for the inner wrapper
       };
 
-    // Default selectors
-    nav = getElement(tinyNav.nav) || getElement("nav"); // String: id of the nav
-    navInner = getElement(tinyNav.navInner) || getElement("nav-inner"); // String: id of the nav wrapper
+      // User defined options
+      for (i in options) {
+        this.options[i] = options[i];
+      }
 
-    // Add closed class for nav
-    nav.className = nav.className + " closed";
+      this.wrapper.className = this.wrapper.className + " closed";
 
-    // Create navigation toggle
-    var toggle = doc.createElement("a");
-    toggle.setAttribute("href", "#");
-    toggle.setAttribute("id", "tinynav-toggle");
-    toggle.innerHTML = label;
-    nav.parentNode.insertBefore(toggle, nav.nextSibling);
-    navToggle = getElement("tinynav-toggle");
+      // Fixes overflow: hidden; bug in Opera Mobile
+      if (ua.match(/(Opera Mobi)/)) {
+        this.wrapper.style.position = "absolute";
+      }
 
-    // Fixes overflow: hidden; bug in Opera Mobile
-    if (ua.match(/(Opera Mobi)/)) {
-      nav.style.position = "absolute";
-    }
+      var inner = this.options.navInner;
+      this.wrapper.inner = typeof inner === "string" ? doc.querySelector(inner) : inner;
 
-    // Debug
-    if (debug === true) {
-      c.log("Navigation toggle created");
-    }
+      // Init
+      TinyNav.prototype.init(this);
 
-    // Determines if we should open or close the nav
-    var navFunction = function (e) {
-        e.preventDefault();
-        if (!nav_open) {
-          nav.className = nav.className.replace(closed, opened);
-          if (computed) {
-            nav.setAttribute(aria, false);
-          }
-          nav_open = true;
+      // Debug
+      if (this.options.debug) {
+        c.log("Inited Tinynav2.js");
+      }
+    };
 
-          // Debug
-          if (debug === true) {
-            c.log("Opened navigation");
-          }
+  TinyNav.prototype = {
 
-        } else {
-          nav.className = nav.className.replace(opened, closed);
-          if (computed) {
-            nav.setAttribute(aria, true);
-          }
-          nav_open = false;
+    init: function (obj) {
+      if (this.initiated) {
+        return;
+      }
+      this.initiated = true;
+      this._createStyles(obj);
+      this._createToggle(obj);
+      function checkResize() {
+        TinyNav.prototype._resizer(obj);
+      }
+      addEvent(window, "load", checkResize);
+      addEvent(window, "resize", checkResize);
+    },
 
-          // Debug
-          if (debug === true) {
-            c.log("Closed navigation");
-          }
+    destroy: function () {
+      this.initiated = false;
+      styleEl.parentNode.removeChild(styleEl);
+      this.wrapper.className = this.wrapper.className.replace(/(^|\s)closed(\s|$)/, " ");
+      this.wrapper.tinynav = null;
+      //this.wrapper.removeAttribute(aria);
+      this._removeToggle();
+      removeEvent(window, "load", checkResize);
+      removeEvent(window, "resize", checkResize);
+    },
 
+    toggle: function (obj) {
+      if (!nav_open) {
+        this.wrapper.className = this.wrapper.className.replace(closed, opened);
+        if (computed) {
+          this.wrapper.setAttribute(aria, false);
         }
+        nav_open = true;
+        if (this.options.debug) {
+          c.log("Opened navigation");
+        }
+      } else {
+        this.wrapper.className = this.wrapper.className.replace(opened, closed);
+        if (computed) {
+          this.wrapper.setAttribute(aria, true);
+        }
+        nav_open = false;
+        if (this.options.debug) {
+          c.log("Closed navigation");
+        }
+      }
+      return false;
+    },
+
+
+    _createStyles: function (obj) {
+      if (!styleEl.parentNode) {
+        head.appendChild(styleEl);
+        if (obj.options.debug) {
+          c.log("Created 'styleEl' to <head>");
+        }
+      }
+    },
+
+    _removeStyles: function (obj) {
+      if (styleEl.parentNode) {
+        styleEl.parentNode.removeChild(styleEl);
+        if (obj.options.debug) {
+          c.log("Removed 'styleEl' from <head>");
+        }
+      }
+    },
+
+    _createToggle: function (obj) {
+      var toggle = doc.createElement("a");
+      toggle.setAttribute("href", "#");
+      toggle.setAttribute("id", "tinynav-toggle");
+      toggle.innerHTML = obj.options.label;
+      obj.wrapper.parentNode.insertBefore(toggle, obj.wrapper.nextSibling);
+      navToggle = doc.querySelector("#tinynav-toggle");
+      if (obj.options.debug) {
+        c.log("Navigation toggle created");
+      }
+      this._handleToggleStates(obj);
+    },
+
+    _removeToggle: function (obj) {
+      navToggle.parentNode.removeChild(navToggle);
+      if (obj.options.debug) {
+        c.log("Navigation toggle removed");
+      }
+    },
+
+    _handleToggleStates: function (obj) {
+      // Mousedown
+      navToggle.onmousedown = function (event) {
+        event.preventDefault();
+        obj.wrapper.tinynav.toggle();
+        if (obj.options.debug) {
+          c.log("Detected mousedown");
+        }
+      };
+
+      // Touchstart event fires before the mousedown event
+      // and can wipe the previous mousedown event
+      navToggle.ontouchstart = function (event) {
+        navToggle.onmousedown = null;
+        event.preventDefault();
+        obj.wrapper.tinynav.toggle();
+        if (obj.options.debug) {
+          c.log("Detected touchstart");
+        }
+      };
+
+      // On click
+      navToggle.onclick = function () {
         return false;
       };
+    },
 
-    // Init on load
-    w.resizer();
+    _resizer: function (obj) {
+      if (computed) {
+        if (window.getComputedStyle(navToggle, null).getPropertyValue("display") !== "none") {
+          navToggle.setAttribute(aria, false);
+          obj.wrapper.setAttribute(aria, true);
 
-    // Mousedown
-    navToggle.onmousedown = function (e) {
+          this._createStyles(obj);
 
-      // Debug
-      if (debug === true) {
-        c.log("Detected mousedown");
+          var savedHeight = obj.wrapper.inner.offsetHeight,
+            innerStyles = "#nav.opened{max-height:" + savedHeight + "px }";
+          styleEl.innerHTML = innerStyles;
+          innerStyles = '';
+          if (obj.options.debug) {
+            c.log("Calculated max-height of " + savedHeight + " pixels and updated 'styleEl'");
+          }
+        } else {
+          navToggle.setAttribute(aria, true);
+          obj.wrapper.setAttribute(aria, false);
+
+          this._removeStyles(obj);
+        }
       }
+    }
 
-      navFunction(e);
-    };
+  };
 
-    // Touchstart event fires before the mousedown event
-    // and can wipe the previous mousedown event
-    navToggle.ontouchstart = function (e) {
-
-      // Debug
-      if (debug === true) {
-        c.log("Detected touchstart");
-      }
-
-      navToggle.onmousedown = null;
-      navFunction(e);
-    };
-
-    // On click
-    navToggle.onclick = function () {
-      return false;
-    };
-  }
-
-  // Run on domready (w.load as a fallback)
-  if (w.addEventListener) {
-    w.addEventListener("DOMContentLoaded", function () {
-      w.navigation();
-      // Run once only
-      w.removeEventListener("load", w.navigation, false);
-    }, false);
-    w.addEventListener("load", w.navigation, false);
-    w.addEventListener("resize", w.resizer, false);
-  } else if (w.attachEvent) {
-    w.attachEvent("onload", w.navigation);
-    w.attachEvent("resize", w.resizer);
-  }
-
-})(this);
+  return TinyNav;
+})(window, document);
---
+++ old__TinyNav2.js
@@ -0,0 +1,203 @@
+/*! tinyNav2.js v0.65
+ * https://github.com/viljamis/tinyNav2.js
+ * http://tinynav2.viljamis.com
+ *
+ * Copyright (c) 2013 @viljamis
+ * Available under the MIT license
+ */
+
+/*jslint browser: true, sloppy: true, vars: true, plusplus: true, indent: 2 */
+
+(function (w) {
+
+  var tinyNav = w.tinyNav || {};
+
+  var nav,
+    navToggle,
+    navInner,
+    c = console,
+    doc = w.document,
+    aria = "aria-hidden",
+    ua = navigator.userAgent,
+    head = doc.getElementsByTagName("head")[0],
+    styleEl = doc.createElement("style"),
+    computed = w.getComputedStyle ? true : false,
+    label = tinyNav.label || "Menu",
+    debug = tinyNav.debug || false; // Boolean: log debug messages to console, true or false
+
+  // Debug
+  if (debug === true) {
+    c.log("Inited Tinynav2.js");
+  }
+
+  // Create style element
+  head.appendChild(styleEl);
+
+  // Debug
+  if (debug === true) {
+    c.log("Created initial 'styleEl' to <head>");
+  }
+
+  // Resizer
+  w.resizer = function () {
+    if (computed) {
+      if (w.getComputedStyle(navToggle, null).getPropertyValue("display") !== "none") {
+
+        // Set aria-hidden
+        navToggle.setAttribute(aria, false);
+        nav.setAttribute(aria, true);
+
+        // Inject custom styles
+        if (!styleEl.parentNode) {
+          head.appendChild(styleEl);
+
+          // Debug
+          if (debug === true) {
+            c.log("Appended 'styleEl' to <head>");
+          }
+
+        }
+        var savedHeight = navInner.offsetHeight,
+          innerStyles = "#nav.opened{max-height:" + savedHeight + "px }";
+        styleEl.innerHTML = innerStyles;
+        innerStyles = '';
+
+        // Debug
+        if (debug === true) {
+          c.log("Calculated max-height of " + savedHeight + " pixels and updated 'styleEl'");
+        }
+
+      } else {
+
+        // Set aria-hidden
+        navToggle.setAttribute(aria, true);
+        nav.setAttribute(aria, false);
+
+        // Remove custom styles
+        if (styleEl.parentNode) {
+          styleEl.parentNode.removeChild(styleEl);
+
+          // Debug
+          if (debug === true) {
+            c.log("Removed 'styleEl' from <head>");
+          }
+
+        }
+      }
+    }
+  };
+
+  // Navigation
+  w.navigation = function () {
+
+    var nav_open = false,
+      closed = "closed",
+      opened = "opened",
+      getElement = function (el) {
+        return doc.getElementById(el);
+      };
+
+    // Default selectors
+    nav = getElement(tinyNav.nav) || getElement("nav"); // String: id of the nav
+    navInner = getElement(tinyNav.navInner) || getElement("nav-inner"); // String: id of the nav wrapper
+
+    // Add closed class for nav
+    nav.className = nav.className + " closed";
+
+    // Create navigation toggle
+    var toggle = doc.createElement("a");
+    toggle.setAttribute("href", "#");
+    toggle.setAttribute("id", "tinynav-toggle");
+    toggle.innerHTML = label;
+    nav.parentNode.insertBefore(toggle, nav.nextSibling);
+    navToggle = getElement("tinynav-toggle");
+
+    // Fixes overflow: hidden; bug in Opera Mobile
+    if (ua.match(/(Opera Mobi)/)) {
+      nav.style.position = "absolute";
+    }
+
+    // Debug
+    if (debug === true) {
+      c.log("Navigation toggle created");
+    }
+
+    // Determines if we should open or close the nav
+    var navFunction = function (e) {
+        e.preventDefault();
+        if (!nav_open) {
+          nav.className = nav.className.replace(closed, opened);
+          if (computed) {
+            nav.setAttribute(aria, false);
+          }
+          nav_open = true;
+
+          // Debug
+          if (debug === true) {
+            c.log("Opened navigation");
+          }
+
+        } else {
+          nav.className = nav.className.replace(opened, closed);
+          if (computed) {
+            nav.setAttribute(aria, true);
+          }
+          nav_open = false;
+
+          // Debug
+          if (debug === true) {
+            c.log("Closed navigation");
+          }
+
+        }
+        return false;
+      };
+
+    // Init on load
+    w.resizer();
+
+    // Mousedown
+    navToggle.onmousedown = function (e) {
+
+      // Debug
+      if (debug === true) {
+        c.log("Detected mousedown");
+      }
+
+      navFunction(e);
+    };
+
+    // Touchstart event fires before the mousedown event
+    // and can wipe the previous mousedown event
+    navToggle.ontouchstart = function (e) {
+
+      // Debug
+      if (debug === true) {
+        c.log("Detected touchstart");
+      }
+
+      navToggle.onmousedown = null;
+      navFunction(e);
+    };
+
+    // On click
+    navToggle.onclick = function () {
+      return false;
+    };
+  }
+
+  // Run on domready (w.load as a fallback)
+  if (w.addEventListener) {
+    w.addEventListener("DOMContentLoaded", function () {
+      w.navigation();
+      // Run once only
+      w.removeEventListener("load", w.navigation, false);
+    }, false);
+    w.addEventListener("load", w.navigation, false);
+    w.addEventListener("resize", w.resizer, false);
+  } else if (w.attachEvent) {
+    w.attachEvent("onload", w.navigation);
+    w.attachEvent("resize", w.resizer);
+  }
+
+})(this);
